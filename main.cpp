22112df0f153fb7963f25ae94cb814732c908874
Sinitsin_Ivan <vad5sz@mail.ru>
2025-02-13 19:09:31 +0300

    Not yet ready Factorization

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@
 long long Polynom::GF = LLONG_MAX - 1;
 bool Polynom::GFEnabled = false;
 
-vector<Polynom> h, v, gs;
+vector<Polynom> h, v, gs, edf;
+
+long long power(long long a, long long exp) {
+    long long result = a;
+    for(int i = 1; i < exp; i++) {
+        result *= a;
+    }
+    return result;
+}
+
 
 Polynom repeated_squaring(Polynom a, long long n, Polynom f){
     Polynom result = Polynom({1});
@@ -19,38 +28,113 @@ Polynom repeated_squaring(Polynom a, long long n, Polynom f){
     return result % f;
 }
 
-vector<Polynom> factor_polynomial_over_finite_field(Polynom f, long long q) {
-    Polynom::setGF(q);
-    return {};
+Polynom equal_degree_splitting(Polynom f, int d) {
+    Polynom a = random(f.degree());
+    if(a.degree() == 0) {
+        return Polynom({0});
+    }
+
+    Polynom g1 = gcd(a, f);
+
+    if(g1 != 1) {
+        return g1;
+    }
+
+    Polynom b = repeated_squaring(a, (power(a.getGF(), d) - 1) / 2, f);
+    Polynom g2 = gcd(b - 1, f);
+    if(g2 != 1 && g2 != f) {
+        return g2;
+    } else {
+        return Polynom({0});
+    }
+}
+
+
+void equal_degree_factorization(Polynom f, int d) {
+    if (d == f.degree()) {
+        edf.push_back(f);
+    }
+    Polynom g({0});
+    while(g == 0){
+        g = equal_degree_splitting(f, d);
+    }
+
+    equal_degree_factorization(g, d);
+    equal_degree_factorization(f/g, d);
+    return;
+}
+
+vector<pair<Polynom, long long>> factor_polynomial_over_finite_field(Polynom f, long long q) {
+
+    h.push_back(Polynom({0, 1}));
+    v.push_back(f / f.leading_coefficient());
+    int i = 0;
+    vector<pair<Polynom, long long>> U;
+    while(v[i] != 1) {
+        i++;
+        h.push_back(repeated_squaring(h[i - 1], q, f));
+        Polynom test = h[i] - h[0];
+        Polynom test2 = h[i];
+        Polynom tesst0 = h[0];
+        Polynom g = gcd(h[i] - h[0], v[i - 1]);
+        if (g == 1) {
+            break;
+        }
+        if (g != 1) {
+            equal_degree_factorization(g, i);
+            vector<Polynom> factors = edf;
+            edf.clear();
+            v.push_back(v[i - 1]);
+            for (Polynom& g_j : factors) {
+                int e = 0;
+                while (v[i] % g_j == 0) {
+                    v[i] /= g_j;
+                    e++;
+                }
+                U.push_back({g_j, e});
+            }
+        }
+    }
+    return U;
 }
 
 
 int main(){
     gs.push_back(Polynom({1}));
+    Polynom::setGF(31);
     Polynom::enableGF();
-    Polynom::setGF(5);
     Polynom a =  Polynom({1, 4, 6, 4, 1}, "a(x)");
-    vector<long long> f(32, 0);
-    f[2] = -1;
+    vector<long long> f(31, 0);
     f.push_back(1);
     Polynom fP(f);
     Polynom b =  Polynom({2, 2, 1}, "b(x)");
+    cout << gcd(fP - Polynom({0, 1}), a) << endl;
+    //vector<pair<Polynom, long long>> factors = factor_polynomial_over_finite_field(a, 31);
     cout << a << endl;
     cout << b << endl;
-    if(a == b) {
+    if(a != 1) {
         cout << "Equal";
     } else {
         cout << "Not Equal";
     }
     cout << endl;
-    cout << Polynom(a + b, "a(x) + b(x)");
+    a += 2;
+    cout << Polynom(a + 2, "a(x) + b(x)");
     cout << endl;
-    cout << Polynom(a - b, "a(x) - b(x)");
+    a -= 2;
+    cout << Polynom(a - 2, "a(x) - b(x)");
     cout << endl;
-    cout << Polynom(a * b, "a(x) * b(x)");
+    a *= 2;
+    cout << Polynom(a * 2, "a(x) * b(x)");
     cout << endl;
-    cout << Polynom(a / b, "a(x) / b(x)");
+    a /= 2;
+    cout << Polynom(a / 2, "a(x) / b(x)");
     cout << endl;
-    cout << Polynom(a % b, "a(x) % b(x)");
+    a %= 2;
+    cout << Polynom(a % 2, "a(x) % b(x)") << endl;
+    // for(int i =0; i < 10; i++) {
+    //     cout << power(2, 3) << endl;
+    // }
+    
     return 0;
 }
--- polynomes.h
+++ polynomes.h
@@ -40,6 +40,10 @@ class Polynom {
             GF = p;
         }
 
+        static long long getGF() {
+            return GF;
+        }
+
         void normaliseGF(){
             for(auto& k : coeffs){ 
                 k = ((k % GF) + GF) % GF;
@@ -50,13 +54,16 @@ class Polynom {
             GF = INT32_MAX;
         }
 
-        Polynom operator=(vector<long long> in) {
-            this->coeffs = in;
+        Polynom& operator=(Polynom const &other) {
+            if (this != &other) { 
+                this->coeffs = other.coeffs;
+            }
             return *this;
         }
-
-        Polynom operator=(Polynom const &other) {
-            this->coeffs = other.coeffs;
+    
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator=(T other) {
+            this->coeffs = {static_cast<long long>(other)};
             return *this;
         }
 
@@ -69,15 +76,31 @@ class Polynom {
             return equal(this->coeffs.begin(), this->coeffs.end(), other.coeffs.begin());
         }
 
+        template<typename T, typename = enable_if_t<is_arithmetic_v<T>>>
+        bool operator==(T other) {
+            if(this->coeffs.size() == 1) {
+                return this->coeffs[0] == static_cast<long long>(other) ? true : false;
+            }
+            return false;
+        }
+
         bool operator!=(Polynom const& other) {
             return !(*this == other);
         }
 
-        Polynom operator+(Polynom const& other) {
+        template<typename T, typename = enable_if_t<is_arithmetic_v<T>>>
+        bool operator!=(T other) {
+            if(this->coeffs.size() == 1) {
+                return this->coeffs[0] == static_cast<long long>(other) ? false : true;
+            }
+            return true;
+        }
+
+        Polynom operator+(Polynom const& other) const{
             bool sizeComp = size(this->coeffs) < size(other.coeffs);
             Polynom result = sizeComp ? other : *this;
             Polynom lesserPolynom = sizeComp ? *this : other;
-            for(long long i = 0; i <= lesserPolynom.power(); i++) {
+            for(long long i = 0; i <= lesserPolynom.degree(); i++) {
                 result[i] += lesserPolynom[i];
             }
             result.trimLeadingZeroes();
@@ -86,11 +109,25 @@ class Polynom {
         }
 
 
-        void operator+=(Polynom const& other) {
-            *this = *this + other;    
+        Polynom& operator+=(Polynom const& other) {
+            *this = *this + other;
+            return *this;    
         }
 
-        Polynom operator-(Polynom const& other) {
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom operator+(T other) const {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            return *this + otherP;
+        }
+
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator+=(T other) {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            *this = *this + otherP;
+            return *this;
+        }
+
+        Polynom operator-(Polynom const& other) const{
             Polynom result = *this;
             if(result.coeffs.size() < other.coeffs.size()){
                 result.coeffs.resize(other.coeffs.size(), 0);
@@ -106,9 +143,22 @@ class Polynom {
             *this = *this - other;    
         }
 
-        Polynom operator* (Polynom const& other) {
-            Polynom result(vector<long long>(power() + size(other.coeffs), 0));
-            for(long long i = 0; i <= power(); i++) {
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom operator-(T other) const {
+            const Polynom otherP = Polynom({static_cast<long long>(other)});
+            return *this - otherP;
+        }
+
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator-=(T other) {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            *this = *this - otherP;
+            return *this;
+        }
+
+        Polynom operator* (Polynom const& other) const {
+            Polynom result(vector<long long>(degree() + size(other.coeffs), 0));
+            for(long long i = 0; i <= degree(); i++) {
                 for(long long j = 0; j < size(other.coeffs); j++){
                     result[i + j] = (result[i + j] + coeffs[i] * other.coeffs[j]);
                     if(GFEnabled) {
@@ -121,19 +171,29 @@ class Polynom {
             return result;
         }
 
-        Polynom operator*(long long other) {
-            return *this * Polynom({other});    
+        Polynom& operator*=(Polynom const& other) {
+            *this = *this * other;
+            return *this;    
         }
 
-        void operator*=(Polynom const& other) {
-            *this = *this * other;    
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom operator*(T other) const {
+            const Polynom otherP = Polynom({static_cast<long long>(other)});
+            return *this * otherP;
         }
 
-        Polynom operator/ (Polynom const& other) {
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator*=(T other) {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            *this = *this * otherP;
+            return *this;
+        }
+
+        Polynom operator/ (Polynom const& other) const{
             return division(*this, other).first;
         }
 
-        Polynom operator/ (long long number) {
+        Polynom operator/ (long long number) const{
             return *this / Polynom({number});
         }
 
@@ -141,12 +201,38 @@ class Polynom {
             *this = *this / other;    
         }
 
-        Polynom operator% (Polynom const& other) {
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom operator/(T other) const {
+            const Polynom otherP = Polynom({static_cast<long long>(other)});
+            return *this / otherP;
+        }
+
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator/=(T other) {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            *this = *this / otherP;
+            return *this;
+        }
+
+        Polynom operator% (Polynom const& other) const{
             return division(*this, other).second;
         }
 
         void operator%=(Polynom const& other) {
             *this = *this % other;    
+        }
+
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom operator%(T other) const {
+            const Polynom otherP = Polynom({static_cast<long long>(other)});
+            return *this % otherP;
+        }
+
+        template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+        Polynom& operator%=(T other) {
+            Polynom otherP = Polynom({static_cast<long long>(other)});
+            *this = *this % otherP;
+            return *this;
         }
 
         Polynom operator++() {
@@ -157,21 +243,21 @@ class Polynom {
             return *this - Polynom({1});
         }
 
-        pair<Polynom, Polynom> division (const Polynom &divisable, const Polynom &divisor){
+        pair<const Polynom, const Polynom> division (const Polynom &divisable, const Polynom &divisor) const{
             if (divisable.coeffs.size() < divisor.coeffs.size()) {
-                return {Polynom(), divisor};
+                return {Polynom(), divisable};
             }
             Polynom div(vector<long long>(divisable.coeffs.size() - divisor.coeffs.size() + 1, 0));
             Polynom result = divisable, mod(vector<long long>(divisable.coeffs.size(), 0));
-            long long dMP = divisor.coeffs.size() - 1; //divisorMaxPower
-            long long inverse = calculateInverse(divisor.coeffs[dMP], GF-2, GF);
+            long long dD = divisor.coeffs.size() - 1; //divisorDegree
+            long long inverse = calculateInverse(divisor.coeffs[dD], GF-2, GF);
             
 
-            for(long long i = divisable.coeffs.size() - 1; i >= 0 && dMP <= i; i--) {
+            for(long long i = divisable.coeffs.size() - 1; i >= 0 && dD <= i; i--) {
                 long long k1 = (result.coeffs[i] * inverse) % GF;
-                div.coeffs[i - dMP] = GFEnabled ? k1 : result.coeffs[i] / divisor.coeffs[dMP];
-                Polynom singlePolynom(vector<long long>(i - dMP, 0));
-                singlePolynom.coeffs.push_back(GFEnabled ? 1 : result.coeffs[i] / divisor.coeffs[dMP]);
+                div.coeffs[i - dD] = GFEnabled ? k1 : result.coeffs[i] / divisor.coeffs[dD];
+                Polynom singlePolynom(vector<long long>(i - dD, 0));
+                singlePolynom.coeffs.push_back(GFEnabled ? 1 : result.coeffs[i] / divisor.coeffs[dD]);
                 result -=  GFEnabled ? ((singlePolynom * divisor) * k1) : singlePolynom * divisor;
                 if(size(result.coeffs) == i + 1) {
                     mod.coeffs[i] += result.coeffs[i];
@@ -181,12 +267,14 @@ class Polynom {
                 i = result.coeffs.size();
             }
             mod += result;
+            div.trimLeadingZeroes();
+            mod.trimLeadingZeroes();
             return {div, mod};
         }
 
         friend ostream& operator<<(ostream& os, Polynom p);
 
-        long long calculateInverse(long long a, long long n, long long mod){
+        long long calculateInverse(long long a, long long n, long long mod) const{
             long long result = 1;
             while(n > 0) {
                 if (n % 2 == 1) {
@@ -205,25 +293,36 @@ class Polynom {
         }
 
         
-        long long power() {
+        long long degree() const {
             return coeffs.size() - 1;
         }
 
 
         long long leading_coefficient() {
-            return this->coeffs.at(power());
-        }
-        
-        Polynom gcd(Polynom a, Polynom b) {
-            while (!b.coeffs.empty()) {
-                Polynom temp = a % b;
-                a = b;
-                b = temp;
-            }
-            return a;
+            return this->coeffs.at(degree());
         }
 
+        friend Polynom gcd(Polynom a, Polynom b);
+        friend Polynom random(long long degree);
 };
+
+Polynom random(long long degree) {
+    Polynom a;
+    for(int i = rand() % degree; i > 0; i--) {
+        a.coeffs.push_back(rand() % a.GF);
+    }
+    a.trimLeadingZeroes();
+    return a;
+}
+
+Polynom gcd(Polynom a, Polynom b) {
+    while (!b.coeffs.empty()) {
+        Polynom temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
 
 ostream& operator<<(ostream& os, Polynom p) {
         os << p.name << " = ";
